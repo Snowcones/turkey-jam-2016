92bd2dab162373465e964d7d1389377b59331d6c
William Henning <wchenningwchenning@gmail.com>
2016-11-19 15:50:44 -0500

    Scene changes when last zombie killed

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
-
 
 
 public class Zombie : MonoBehaviour
 {
-    public float zombiesdead = 0f;
+	public string animation;
+    public bool isDead = false;
     public AudioSource zombie;
 
     // Use this for initialization
@@ -19,13 +20,46 @@ public class Zombie : MonoBehaviour
     {
 
     }
+
+	bool AllZombiesDead() {
+		GameObject[] zombieGameObjs = GameObject.FindGameObjectsWithTag("zombie");
+		Zombie[] zombies = new Zombie[zombieGameObjs.Length];
+
+		for(int i=0; i<zombieGameObjs.Length; i++) {
+			zombies[i] = zombieGameObjs[i].GetComponent<Zombie>();
+		}
+
+		for(int i=0; i<zombieGameObjs.Length; i++) {
+			if(!zombies[i].isDead) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	void nextScene() {
+		int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+		if(nextScene < SceneManager.sceneCountInBuildSettings) {
+            //Load the next level
+            SceneManager.LoadScene(nextScene);
+      	} else {
+			//At end of game
+            Debug.Log(SceneManager.sceneCount);
+    	}
+	}
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Bullet")
         {  //Destroy(gameObject);
             zombie.Play();
-            GetComponent<Animator>().SetTrigger("dead 0");
-                        
+            GetComponent<Animator>().SetTrigger(animation);
+            isDead = true;
+
+            //Checks to see if the level is cleared
+            if(AllZombiesDead()) {
+            	Invoke("nextScene", 2);
+            }
         }
     }
 }
